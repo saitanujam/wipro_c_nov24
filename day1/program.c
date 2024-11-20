e5d3dfd6619328bb048d56c5fd33306564bd38af
saitanujam <tanusai2001@gmail.com>
2024-11-20 23:53:10 +0530

    first program

---
+++ day1/program.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch;
+    int num;
+    float fnum;
+    double dnum;
+    short int snum;
+    long int lnum;
+    long long int llnum;
+    printf("Size of char (ch) = %d \n", sizeof(char));                      // sizeof(ch)
+    printf("Size of int (num) = %d \n", sizeof(num));                       // sizeof(ch)
+
+}
